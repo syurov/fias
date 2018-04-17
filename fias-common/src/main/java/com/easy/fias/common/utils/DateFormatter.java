e01b850fe28e656198836bd1a871f6050d2e40eb
SYurov <yurovsa@mail.ru>
2018-04-17 22:32:10 +0300

    add common module

---
+++ fias-common/src/main/java/com/easy/fias/common/utils/DateFormatter.java
@@ -0,0 +1,26 @@
+package com.easy.fias.common.utils;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateFormatter {
+  private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+  public static SimpleDateFormat getDateFormat(){
+    return  simpleDateFormat;
+  }
+
+  public static String format(Timestamp timestamp) {
+    if (timestamp == null) {
+      return null;
+    }
+    return simpleDateFormat.format(timestamp);
+  }
+
+  public static Timestamp parse(String source) throws ParseException {
+    if (source == null)
+      return null;
+    return new Timestamp(simpleDateFormat.parse(source).getTime());
+  }
+}
---
+++ fias-common/src/main/java/com/easy/fias/common/utils/StringToTimestampConverter.java
@@ -0,0 +1,22 @@
+package com.easy.fias.common.utils;
+
+import com.easy.fias.common.utils.DateFormatter;
+import org.apache.log4j.Logger;
+import org.springframework.core.convert.converter.Converter;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+
+public class StringToTimestampConverter implements Converter<String, Timestamp> {
+  private final Logger logger = Logger.getLogger(StringToTimestampConverter.class.getName());
+
+  @Override
+  public Timestamp convert(String source) {
+    try {
+      return DateFormatter.parse(source);
+    } catch (ParseException e) {
+      logger.error(e);
+    }
+    return null;
+  }
+}
